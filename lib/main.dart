abfd849101407bf4aa67a9e8474757a0cae9575b
Le Anh Chien <chienla.ptit@gmail.com>
2020-03-02 21:04:08 +0900

    Add action ListView'

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,18 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _words = <WordPair>[];
-  
+  final List<WordPair> _words = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = new Set<WordPair>();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     final wordPair = WordPair.random();
     return ListView.builder(
         itemBuilder: (context,index) {
+          if (index.isOdd) {
+            return Divider();
+          }
           if (index>=_words.length) {
             _words.addAll(generateWordPairs().take(10));
           }
@@ -42,11 +46,24 @@ class RandomWordsState extends State<RandomWords> {
     });
   }
   Widget _buildRow(WordPair wordPair) {
+    final bool alreadySaved = _saved.contains(wordPair);
     return ListTile(
       title: Text(
         wordPair.asPascalCase,
-        style: const TextStyle(fontSize: 18.0),
+        style: _biggerFont ,
       ),
+      trailing: new Icon(
+        alreadySaved? Icons.favorite: Icons.favorite_border,
+        color: alreadySaved? Colors.red: null,),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(wordPair);
+          } else {
+            _saved.add(wordPair);
+          }
+        });
+      },
     );
   }
 }
