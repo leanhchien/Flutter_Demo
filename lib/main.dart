aeb7e7a012e7122c24c8a8432dcb82659cfea31b
Le Anh Chien <chienla.ptit@gmail.com>
2020-03-02 20:23:53 +0900

    Material Design

--- lib/main.dart
+++ lib/main.dart
@@ -4,62 +4,22 @@ import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "My App",
-    home: MyScaffold(),
+    title: "Flutter Tutorial",
+    home: TutorialHome(),
   ));
 }
-
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-  final Widget title;
-  @override
-  Widget build(BuildContext context) {
-
-    // TODO: implement build
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.deepOrange),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigation Menu",
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: "Search",
-            onPressed: null,
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(title: Text(
-            "Example Title",
-            style: Theme.of(context).primaryTextTheme.title,
-          ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text("Hello"),
-            ),
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu),tooltip: "Navigation Menu",onPressed: null,),
+        title: Center(child: Text("Hello"),),
+        actions: <Widget>[IconButton(icon: Icon(Icons.search),tooltip: "Search",onPressed: null,)],
       ),
+      body: Center(child: Text("Hello World"),),
+      floatingActionButton: FloatingActionButton(tooltip: "Add",child: Icon(Icons.add),onPressed: null,),
     );
   }
 }
